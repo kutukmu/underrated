2b8ea370d8e33392cbea6f7058cf23e6e4c6c454
Kerim <kutukmu@gmail.com>
2020-10-15 07:53:10 -0500

    path

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sednFİle(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
